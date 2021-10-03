4b85aea3546ea5fb356a3f9ecd2c23eb5bdf0288
Mereevee <71831989+Mereevee@users.noreply.github.com>
2021-10-02 21:44:04 -0300

    tabuleiro

--- game.js
+++ game.js
@@ -1,7 +1,28 @@
+let width = 400;
+let height = 400;
+
+let black, white;
+let x_size = width/8;
+let y_size = height/8;
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
+    createCanvas(width, height);
+    black = color(0, 0, 0);
+    white = color(255, 255, 255);
+    size = 30;
+}
+
+function draw() {
     background(220);
-  }
+    noStroke();
+    for (let y = 0; y < 8; y++) {
+        for (let x = 0; x < 8; x++) {
+            if ((x+y) % 2 == 0) { c = white; }
+            if ((x+y) % 2 == 1) { c = black; }
+
+            fill(c);
+            rect(x * x_size, y* y_size, x_size, y_size);
+        }
+    }
+
+}
